f3cbdb7ca7916ac3023cc40176a0e53262536a6d
kyrylolvov <kyrylolvov@outlook.com>
2021-09-21 21:38:09 -0400

    Added spinner

--- src/containers/Feed.tsx
+++ src/containers/Feed.tsx
@@ -4,8 +4,10 @@ import styled from "styled-components";
 
 import Logo from "../components/Logo";
 import PostItem from "../components/PostItem";
+import BeatLoader from "react-spinners/BeatLoader";
 
 import { getPosts } from "../store/actions/posts";
+import { Post } from "../store/reducers/posts";
 import { selectPosts } from "../store/selectors/posts";
 
 const Feed: React.FC = () => {
@@ -27,19 +29,21 @@ const Feed: React.FC = () => {
       <Header>
         <Logo />
       </Header>
-      <Main>
-        {posts
-          ? posts.map((post) => (
-              <PostItem
-                key={post.url}
-                title={post.title}
-                image={post.url}
-                date={post.date}
-                description={post.explanation}
-                url={post.url}
-              />
-            ))
-          : "Loading"}
+      <Main posts={posts}>
+        {posts ? (
+          posts.map((post) => (
+            <PostItem
+              key={post.url}
+              title={post.title}
+              image={post.url}
+              date={post.date}
+              description={post.explanation}
+              url={post.url}
+            />
+          ))
+        ) : (
+          <BeatLoader color={"#eb5757"} />
+        )}
       </Main>
     </>
   );
@@ -47,7 +51,8 @@ const Feed: React.FC = () => {
 
 const Header = styled.div``;
 
-const Main = styled.div`
+const Main = styled.div<{ posts: Post[] | null }>`
+  ${({ posts }) => (posts ? "" : "text-align: center")};
   display: grid;
   grid-template-columns: 1fr;
   gap: 10px;
